d5a6490860ffc3657ea4f0efd8df6ccea67d51a7
Francisco Arrieta <francisarrna@gmail.com>
2026-02-13 13:31:37 +0100

    Cambios en el backend en el apartado de actualizar usuarios para imagenes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -280,15 +280,33 @@ function updateUser(req, res){
         return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
     }
 
-    User.findByIdAndUpdate(userId, update, {new:true})
-        .then((userUpdated) => {
-            if(!userUpdated) return res.status(500).send({message: 'No se ha podido actualizar el usuario'});
+    User.find({
+            $or: [
+                { email: update.email.toLowerCase() },
+                { nick: update.nick.toLowerCase() }
+            ]
+        }).exec()
+            .then((users) => {
+                var user_isset = false;
 
-            res.status(200).send({ user: userUpdated})
-        })
-        .catch((err) => {
-            if(err) return res.status(500).send({message: 'Error en la petición'});
-        }); 
+                users.forEach((user) => {
+                    if (user && user._id != userId) {
+                        user_isset = true;
+                    }
+                })
+                
+                if(user_isset) return res.status(404).send({ message: 'Los datos ya están en uso' });
+
+                User.findByIdAndUpdate(userId, update, {new:true})
+                .then((userUpdated) => {
+                    if(!userUpdated) return res.status(500).send({message: 'No se ha podido actualizar el usuario'});
+
+                    res.status(200).send({ user: userUpdated})
+                })
+                .catch((err) => {
+                    if(err) return res.status(500).send({message: 'Error en la petición'});
+                }); 
+            });
 }
 
 // Subir archivo de imagen/avatar de usuario
