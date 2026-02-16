536e7f4819eadc94e96fd161fdaf20a38da0f0c3
Francisco Arrieta <francisarrna@gmail.com>
2026-02-16 12:21:16 +0100

    Componente sidebars creado

---
+++ client/src/app/components/sidebars/sidebars.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebars',
+  imports: [],
+  templateUrl: './sidebars.html',
+  styleUrl: './sidebars.css',
+})
+export class Sidebars {
+
+}
